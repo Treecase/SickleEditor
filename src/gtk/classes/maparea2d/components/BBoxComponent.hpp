18a672e42a02885d1e757056600c9a33d6150a4e
Trevor Last <trevorclast@gmail.com>
2024-04-21 18:34:39 -0400

    MapArea2D object picking uses components

---
+++ src/gtk/classes/maparea2d/components/BBoxComponent.hpp
@@ -0,0 +1,47 @@
+/**
+ * BBoxComponent.hpp - Generic bounding-box component.
+ * Copyright (C) 2024 Trevor Last
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SE_MAPAREA2D_BBOXCOMPONENT_HPP
+#define SE_MAPAREA2D_BBOXCOMPONENT_HPP
+
+#include <editor/interfaces/Component.hpp>
+#include <gtk/classes/maparea2d/MapArea2D.hpp>
+#include <utils/BoundingBox.hpp>
+
+
+namespace World2D
+{
+    /**
+     * Component to generate bounding boxes for world objects.
+     */
+    class BBoxComponent : public Sickle::Component
+    {
+    public:
+        /**
+         * Get the bounding box of the attached object in drawspace.
+         *
+         * @param maparea The MapArea2D who's drawspace we're using.
+         * @return Bounding box of the object in drawspace.
+         */
+        virtual BBox2 bbox(Sickle::MapArea2D const &maparea) const=0;
+
+        virtual void execute() override {}
+    };
+}
+
+#endif
---
+++ src/gtk/classes/maparea2d/components/BBoxComponentFactory.hpp
@@ -0,0 +1,48 @@
+/**
+ * BBoxComponentFactory.hpp - Factory to construct appropriate BBoxComponents
+ *                            for Sickle world objects.
+ * Copyright (C) 2024 Trevor Last
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SE_MAPAREA2D_BBOXCOMPONENTFACTORY_HPP
+#define SE_MAPAREA2D_BBOXCOMPONENTFACTORY_HPP
+
+#include "BBoxComponent.hpp"
+
+#include <editor/interfaces/EditorObject.hpp>
+
+
+namespace World2D
+{
+    /**
+     * Constructs the correct RenderComponents for various EditorObjects.
+     */
+    class BBoxComponentFactory
+    {
+    public:
+        /**
+         * Construct an appropriate BBoxComponent for the object. Note that the
+         * constructed component is not attached to the object.
+         *
+         * @param object The object to construct a BBoxComponent for.
+         * @return The new component, or nullptr if none are appropriate.
+         */
+        std::shared_ptr<BBoxComponent> construct(
+            Sickle::Editor::EditorObjectRef const &obj);
+    };
+}
+
+#endif
